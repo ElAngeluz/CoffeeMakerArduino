1408e3144a86c775368129888d4b773c3a992125
Sergio <sergi_xavier01@hotmail.com>
2016-02-18 14:58:50 -0500

    Connect with thinkSpeak the proximity sensor
    Update the sensor in 15s to the ThinkSpeak

--- proyecto-cafetera/proyecto-cafetera.ino
+++ proyecto-cafetera/proyecto-cafetera.ino
@@ -19,7 +19,10 @@ SoftwareSerial mySerial(3,2); /* RX:D2, TX:D3 */
 ESP8266 wifi(mySerial);
 
 #define RELAY_PIN 4
+String key = "JOXZJ0MWXKS66ENR"; //Key proporcionado por ThingSpeak
+String head = "GET /update?api_key=";
 String tail = " HTTP/1.0\r\n\r\n";
+int estadoProximidad=0;
 
 void setup()
 {
@@ -59,8 +62,19 @@ void loop()
   int sensorValue = analogRead(A5);
   // print out the value you read:
   Serial.println(sensorValue);
-  delay(100);  
-  
+  delay(10);  
+
+  if(sensorValue<=930){
+    estadoProximidad=1;
+    digitalWrite(RELAY_PIN, HIGH);
+      sendProximidad();
+    
+    }else{
+      estadoProximidad=0;
+      digitalWrite(RELAY_PIN, LOW);
+      sendProximidad();
+      
+    }
   /*int cmd = getOn_Off_State();
 
   
@@ -78,9 +92,41 @@ void loop()
     }*/
 }
 
+void sendProximidad(void){
+    uint8_t buffer[128] = {0};
+    String field = "&field3=";
+    String mensaje= head+key+field+ String(estadoProximidad) +tail;
+    Serial.println(mensaje);
+    
+    if (wifi.createTCP(HOST_NAME, HOST_PORT)) {
+        Serial.print("create tcp ok\r\n");
+    } else {
+        Serial.print("create tcp err\r\n");
+    }
+
+    const char *cmd= mensaje.c_str();
+    wifi.send((const uint8_t*)cmd, strlen(cmd));
+
+    uint32_t len = wifi.recv(buffer, sizeof(buffer), 10000);
+    if (len > 0) {
+        Serial.print("Received:[");
+        for(uint32_t i = 0; i < len; i++) {
+            Serial.print((char)buffer[i]);
+        }
+        Serial.print("]\r\n");
+    }
+
+    if (wifi.releaseTCP()) {
+        Serial.print("release tcp ok\r\n");
+    } else {
+        Serial.print("release tcp err\r\n");
+    }
+    delay(16000);
+  }
+
 int getOn_Off_State(){
   ("http://api.thingspeak.com/channels/81636/fields/1/last");
-uint8_t buffer[1024] = {0};
+  uint8_t buffer[1024] = {0};
 
     if (wifi.createTCP(HOST_NAME, HOST_PORT)) {
         Serial.print("create tcp ok\r\n");
@@ -100,7 +146,7 @@ uint8_t buffer[1024] = {0};
         Serial.print("]\r\n");
     }
 
-    Serial.print("CODIGO:"+hello[0]);
+    Serial.println("CODIGO:"+hello[0]);
 
     if (wifi.releaseTCP()) {
         Serial.print("release tcp ok\r\n");
